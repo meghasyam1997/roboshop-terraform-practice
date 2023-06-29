e174d197cacb100a35a68ec684b9f128beaadbda
meghasyam1997 <meghasyam01@gmail.com>
2023-06-29 13:54:11 +0530

    creating vpc

---
+++ env-dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "terrafrom-syam"
+key    = "practice-roboshop/dev/terrafrom.tfstate"
+region = "us-east-1"
---
+++ main.tf
@@ -0,0 +1,6 @@
+module "vpc" {
+  source = "git::https://github.com/meghasyam1997/tf-module-vpc-practice.git"
+
+  for_each = var.vpc
+  cidr_block = each.value["cidr_block"]
+}
