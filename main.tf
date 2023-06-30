e7c8c6c5f148dc3156937874556fafb5f6194392
meghasyam1997 <meghasyam01@gmail.com>
2023-06-30 12:01:25 +0530

    creating subnets

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@ module "vpc" {
 
   for_each = var.vpc
   cidr_block = each.value["cidr_block"]
+  subnets = each.value["subnets"]
+  name = each.value["name"]
   tags = local.tags
   env = var.env
 }
